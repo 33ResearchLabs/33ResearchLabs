db42ea40260a14a8ce6c0094a82c3633b5e94775
33ResearchLabs <33researchlabs@gmail.com>
2025-08-06 12:50:49 +0530

    issue fixed

--- 33ResearchLabs/src/pages/Admin/NewBlog.tsx
+++ 33ResearchLabs/src/pages/Admin/NewBlog.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Card, CardContent, CardHeader } from "../../Components/UI/Card";
 import { useNavigate } from "react-router-dom";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export const CreateBlogPage = () => {
   const [title, setTitle] = useState("");
